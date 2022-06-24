074c0d38c759643342258289c934593cc30c6f98
zaher-aa <zaherabuamro@gmail.com>
2022-06-24 10:19:14 +0300

    initialized client

---
+++ client/src/App.js
@@ -0,0 +1,43 @@
+import { GoogleLogin } from 'react-google-login';
+import { gapi } from 'gapi-script';
+import { useEffect } from 'react';
+import axios from 'axios';
+
+function App() {
+  useEffect(() => {
+    const start = () => {
+      gapi.client.init({
+        clientId: process.env.REACT_APP_CLIENT_ID,
+        scope: 'email',
+      });
+    };
+
+    gapi.load('client:auth2', start);
+  }, []);
+
+  const onSuccess = async (res) => {
+    const { tokenId } = res;
+
+    try {
+      const backendResponse = await axios.post('/login/google', { tokenId });
+      console.log(backendResponse);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+  const onFailure = (res) => console.log(res);
+
+  return (
+    <div>
+      <GoogleLogin
+        clientId={process.env.REACT_APP_CLIENT_ID}
+        buttonText="Login"
+        onSuccess={onSuccess}
+        onFailure={onFailure}
+        isSignedIn={true}
+      />
+    </div>
+  );
+}
+
+export default App;
